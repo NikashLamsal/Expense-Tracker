d4fe515ae59890ec5b8c94b94d1f073d42410953
NikashDevX <lamsalnikash312@gmail.com>
2026-02-06 14:53:57 +0545

    added authentication

--- expensetracker/tracker/urls.py
+++ expensetracker/tracker/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.index,name="index"),
     path('delete-transaction/<id>/', views.delete_transaction , name="delete_transaction"),
-    path('update_transaction/<id>/',views.update_transaction , name="update_transaction")
+    path('update_transaction/<id>/',views.update_transaction , name="update_transaction"),
+    path('login/', views.login_view,name="login_page"),
+    path('register/', views.register_view,name="register_page"),
+    path('logout/',views.logout_view,name = "logout_page"),
 ]
-  
--- expensetracker/tracker/views.py
+++ expensetracker/tracker/views.py
@@ -3,18 +3,23 @@ from django.shortcuts import get_object_or_404
 from .models import TrackingHistory , CurrentBalance
 from django.db.models import Sum
 from django.contrib import messages
+from django.contrib.auth.models import User
+
+from django.contrib.auth import authenticate , login , logout
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
  
-
+@login_required(login_url="login_page")
 def index(request):  # sourcery skip: assign-if-exp, introduce-default-else
  
-
-    current_balance,_ = CurrentBalance.objects.get_or_create(id = 1)
+ 
+    current_balance,_ = CurrentBalance.objects.get_or_create(user=request.user)
 
     edit_transaction = None
     edit_id = request.GET.get('edit')
     if edit_id:
-        edit_transaction = get_object_or_404(TrackingHistory,id = edit_id)
+        edit_transaction = get_object_or_404(TrackingHistory,id = edit_id,user=request.user)
     if request.method == "POST":
 
         description = request.POST.get('description')
@@ -33,13 +38,14 @@ def index(request):  # sourcery skip: assign-if-exp, introduce-default-else
         # income = 0
         # expense = 0
  
-        TrackingHistory.objects.create(
+        TrackingHistory.objects.create (
+            user = request.user,
             amount = amount,
             expense_type = expense_type,
             current_balance = current_balance,
-            description = description)
+            description = description )
         
-        total = TrackingHistory.objects.aggregate(total=Sum('amount'))['total'] or 0
+        total = TrackingHistory.objects.filter(user = request.user).aggregate(total=Sum('amount'))['total'] or 0
 
         current_balance.current_balance = total
         current_balance.save()
@@ -48,8 +54,9 @@ def index(request):  # sourcery skip: assign-if-exp, introduce-default-else
 
     income = 0
     expense = 0
-
-    for tracking_history in TrackingHistory.objects.all():
+    user_transactions = TrackingHistory.objects.filter(user=request.user)
+   
+    for tracking_history in user_transactions:
          
         if tracking_history.expense_type == "CREDIT":
               income += tracking_history.amount
@@ -60,31 +67,33 @@ def index(request):  # sourcery skip: assign-if-exp, introduce-default-else
     context = {
         'income' : income,
         'expense' : expense,
-        'transactions' : TrackingHistory.objects.all(),
+        'transactions' : user_transactions,
         'current_balance' : current_balance,
         'edit_transaction' : edit_transaction
     }
 
     return render(request, 'index.html' , context)
 
-
+@login_required(login_url="login_page")
 def delete_transaction(request , id):
-    tracking_history = TrackingHistory.objects.filter(id = id)
+    tracking_history = TrackingHistory.objects.filter(id = id, user = request.user)
     if tracking_history.exists():
+        tracking_history = tracking_history.first()
 
-        current_balance,_ = CurrentBalance.objects.get_or_create(id = 1)
-        tracking_history = tracking_history[0]
+        current_balance,_ = CurrentBalance.objects.get_or_create(user = request.user)
+        # tracking_history = tracking_history[0]
         current_balance.current_balance = current_balance.current_balance - tracking_history.amount
         current_balance.save()
 
 
 
-    tracking_history.delete()
+        tracking_history.delete()
     return redirect('/')
 
+@login_required(login_url="login_page")
 def update_transaction(request, id):
 
-    transaction = TrackingHistory.objects.get(id = id)
+    transaction = get_object_or_404(TrackingHistory, id=id, user=request.user)
     if request.method == "POST":
         description = request.POST.get('description')
         amount = request.POST.get('amount')
@@ -96,19 +105,79 @@ def update_transaction(request, id):
         expense_type = "CREDIT"
         if float(amount) < 0:
             expense_type = "DEBIT"
-
+ 
         transaction.description = description
         transaction.amount = amount
         transaction.expense_type = expense_type
         transaction.save()
 
 
-        total = TrackingHistory.objects.aggregate(total=Sum('amount'))['total'] or 0
+        total = TrackingHistory.objects.filter(user=request.user).aggregate(total=Sum('amount'))['total'] or 0
 
-        current_balance,_ = CurrentBalance.objects.get_or_create(id=1)
+        current_balance,_ = CurrentBalance.objects.get_or_create(user=request.user)
         current_balance.current_balance = total
         current_balance.save()
 
         messages.success(request, "Transaction updated successfully")
         return redirect('/')
     return redirect('/')
+
+
+
+def login_view(request):
+    if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = User.objects.filter(username = username)
+            if not user.exists():
+                messages.success(request,"user not found")
+                return redirect('/login/')
+            user = authenticate(username = username , password = password)
+            print(user)
+            if not user:
+                messages.success(request , "Incorrect password")
+
+                return redirect('/login/')
+            login(request , user)
+            return redirect('/')
+
+    return render(request,"login.html" )
+
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/login/')
+
+
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+
+
+        user = User.objects.filter(username = username)
+        if user.exists():
+           messages.success(request,"username already taken")
+           return redirect('/register/')
+        
+        user = User.objects.create(
+            username = username,
+            first_name = first_name,
+            last_name = last_name
+        )
+        user.set_password(password)
+        user.save()
+        messages.success(request,"Account Created")
+        return redirect('/login/')
+
+        
+
+
+
+
+    return render(request,"register.html" )
+
